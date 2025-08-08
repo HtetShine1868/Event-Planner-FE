4edcab0c8733d1d8947137a125fd8a4cb06b6b11
wrenyy14 <urwrenyy@gmail.com>
2025-08-08 12:40:01 +0630

    nav bar and homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
+/*import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navibar from "./components/Navbar";
+import LoginPage from "./features/auth/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import HomePage from "./pages/HomePage";
+const App = () => {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Navibar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;*/
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navibar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
 import LoginPage from "./features/auth/LoginPage";
 import RegisterPage from "./features/auth/RegisterPage";
 import Dashboard from "./pages/Dashboard";
@@ -9,18 +40,22 @@ import ProtectedRoute from "./components/ProtectedRoute";
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <div className="min-h-screen bg-gray-100">
+        <Navibar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
     </Router>
   );
 };
